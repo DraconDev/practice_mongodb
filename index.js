97fabdd0c142553f07f83fd77d0d87708195ba91
DraconDev <dracsharp@gmail.com>
2023-10-17 15:20:43 +0100

    pre dotenv

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const mongoose = require("mongoose");
 main().catch((err) => console.log(err));
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    await mongoose.connect("mongodb+srv://practiceboy:<password>@cluster0.sjim42x.mongodb.net/?retryWrites=true&w=majority");
 
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+// mongodb+srv://practiceboy:<password>@cluster0.sjim42x.mongodb.net/?retryWrites=true&w=majority
+
+const kittySchema = new mongoose.Schema({
+    name: String,
+});
+
+const Kitten = mongoose.model("Kitten", kittySchema);
+
+const silence = new Kitten({ name: "Silence" });
+console.log(silence.name); // 'Silence'
